f31b80b4d72f8a339aa333fab9b50fece6d8462b
Alex <PR1ME-GOP@yandex.ru>
2019-04-27 22:28:36 +0400

    Большие фиксы ошибок

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,40 @@
 $(function() {
 
+   
+    $(".tab").click(function () {
+        $(".content").removeClass("active")
 
-    
+
+
+        if ($(this).hasClass("tab-1")) {
+            
+            if ($(".content-1").hasClass("active")) {
+                $(".content-1").removeClass("active")
+            } else {
+                $(".content-1").addClass("active")
+            }
+        } else if ($(this).hasClass("tab-2")) {
+            if ($(".content-2").hasClass("active")) {
+                $(".content-2").removeClass("active")
+            } else {
+                $(".content-2").addClass("active")
+            }
+        } else if ($(this).hasClass("tab-3")) {
+            if ($(".content-3").hasClass("active")) {
+                $(".content-3").removeClass("active")
+            } else {
+                $(".content-3").addClass("active")
+            }
+        }else if ($(this).hasClass("tab-4")) {
+            if ($(".content-4").hasClass("active")) {
+                $(".content-4").removeClass("active")
+            } else {
+                $(".content-4").addClass("active")
+            }
+        }
+            
+            
+        
+  
+});
 });
