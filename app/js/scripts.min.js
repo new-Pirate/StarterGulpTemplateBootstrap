04f95728593b8f92d7e5122d5b97d40c7545b56d
Alex <PR1ME-GOP@yandex.ru>
2019-07-18 21:39:37 +0400

    Добавил валидацию

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,40 +1,7 @@
-$(function() {
+$(function () {
+    
+    /* Валидация */
 
-   
-    $(".tab").click(function () {
-        $(".content").removeClass("active")
-
-
-
-        if ($(this).hasClass("tab-1")) {
-            
-            if ($(".content-1").hasClass("active")) {
-                $(".content-1").removeClass("active")
-            } else {
-                $(".content-1").addClass("active")
-            }
-        } else if ($(this).hasClass("tab-2")) {
-            if ($(".content-2").hasClass("active")) {
-                $(".content-2").removeClass("active")
-            } else {
-                $(".content-2").addClass("active")
-            }
-        } else if ($(this).hasClass("tab-3")) {
-            if ($(".content-3").hasClass("active")) {
-                $(".content-3").removeClass("active")
-            } else {
-                $(".content-3").addClass("active")
-            }
-        }else if ($(this).hasClass("tab-4")) {
-            if ($(".content-4").hasClass("active")) {
-                $(".content-4").removeClass("active")
-            } else {
-                $(".content-4").addClass("active")
-            }
-        }
-            
-            
-        
-  
-});
+    $('#phone').mask('+0 (000) 000-00-00');
+    $('#growth').mask('000');
 });
